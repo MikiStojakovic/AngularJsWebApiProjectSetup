7578c1651843ea5ccf449997f11abb80b7d821f2
Miroslav Stojakovic <miroslav.miki.stojakovic@gmail.com>
2018-02-07 11:37:40 +0100

    Initial commit

---
+++ AngularJsWebApi.Development/AngularJsWebApi.Development/Program.cs
@@ -0,0 +1,28 @@
+﻿using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Owin;
+using Microsoft.Owin.Hosting;
+using AngularJsWebApi.Frontend;
+
+namespace AngularJsWebApi.Development
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure();
+            var url = ConfigurationManager.AppSettings["hostUrl"];
+
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.Title = "TopologyBarPOC Agent";
+                Console.ReadKey();
+            }
+        }
+    }
+}
